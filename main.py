6cb36072d839944f81351a4541f5f1fc3fdc6bd2
juanky <juan.carlos.recio.abad@gmail.com>
2020-03-08 18:11:41 +0100

    Remove unused endpoint

--- main.py
+++ main.py
@@ -6,10 +6,6 @@ from json_encoder import JSONEncoder
 app = Flask(__name__)
 flightsService = FlightsService()
 
-@app.route('/')
-def index():
-    return 'Index!'
-
 @app.route('/requests', methods = ['GET'])
 def get_flight_requests():
     return (flightsService.find_all(), 200, {'ContentType': 'application/json'})
